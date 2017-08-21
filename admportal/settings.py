b25a157caad7847b55523096995522b62cbfa8e4
Jittat Fakcharoenphol <jittat@gmail.com>
2017-08-21 11:03:49 +0700

    fixed local setting import

--- admportal/settings.py
+++ admportal/settings.py
@@ -105,16 +105,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from admportal.settings_local import *
+except ImportError:
+    pass 
