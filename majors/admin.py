df0e5fe42f10e455f4f668d556360e63f3c05a30
Jittat Fakcharoenphol <jittat@gmail.com>
2017-08-22 10:44:16 +0700

    added models + migrations for AdmissionRound and AdmissionProject

--- majors/admin.py
+++ majors/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Campus, Faculty
+from .models import Campus, Faculty, AdmissionRound, AdmissionProject
 
 admin.site.register(Campus)
 admin.site.register(Faculty)
+admin.site.register(AdmissionRound)
+admin.site.register(AdmissionProject)
--- majors/models.py
+++ majors/models.py
@@ -24,4 +24,32 @@ class Faculty(models.Model):
 
     
 
+class AdmissionRound(models.Model):
+    number = models.IntegerField()
+    subround_number = models.IntegerField(default=0)
+    rank = models.IntegerField()
+
+    class Meta:
+        ordering = ['rank']
+
+        
+    def __str__(self):
+        if self.subround_number == 0:
+            return 'รอบที่ %d' % (self.number,)
+        else:
+            return 'รอบที่ %d/%d' % (self.number, self.subround_number)
+
     
+class AdmissionProject(models.Model):
+    title = models.CharField(max_length=400)
+    short_title = models.CharField(max_length=200)
+    admission_round = models.ForeignKey('AdmissionRound')
+    campus = models.ForeignKey('Campus',
+                               null=True,
+                               blank=True)
+    
+    general_conditions = models.TextField(blank=True)
+    column_descriptions = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
