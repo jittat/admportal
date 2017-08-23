5281ad3c43b640a2bd31d95015b9c36f2ece6d0e
Jittat Fakcharoenphol <jittat@gmail.com>
2017-08-23 22:49:56 +0700

    shows easy first page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from majors.models import Campus, AdmissionRound, AdmissionProject
+
 def index(request):
+    admission_rounds = AdmissionRound.objects.all()
+    campuses = Campus.objects.all()
+    
     return render(request,
                   'main/index.html',
-                  {})
+                  { 'admission_rounds': admission_rounds,
+                    'campuses': campuses, })
