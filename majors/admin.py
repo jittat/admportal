d5bdd410ac7c4df77b48d4c70a9f48647fe2c4f1
Jittat Fakcharoenphol <jittat@gmail.com>
2020-12-20 01:51:58 +0700

    admins enables major visibility

--- majors/admin.py
+++ majors/admin.py
@@ -8,12 +8,19 @@ def make_available(modeladmin, request, queryset):
         project.save()
 make_available.short_description = 'Mark selected project as available'
 
+def make_major_visible(modeladmin, request, queryset):
+    for project in queryset:
+        project.major_detail_visible = True
+        project.save()
+make_major_visible.short_description = 'Show major details'
+
 class AdmissionProjectAdmin(admin.ModelAdmin):
     list_display = ['title',
                     'campus',
-                    'is_available']
+                    'is_available',
+                    'major_detail_visible']
     ordering = ['id']
-    actions = [make_available]
+    actions = [make_available, make_major_visible]
 
 admin.site.register(AdmissionProject, AdmissionProjectAdmin)
 
